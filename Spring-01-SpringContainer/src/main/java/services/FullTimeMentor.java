218f89e8561ea2f6003757bc61d07415852660e2
ececi <huseyinkiceci@gmail.com>
2021-10-21 11:14:09 +0300

    stuff added

---
+++ Spring-01-SpringContainer/src/main/java/services/FullTimeMentor.java
@@ -0,0 +1,10 @@
+package services;
+
+import interfaces.Mentor;
+
+public class FullTimeMentor implements Mentor {
+
+    public void createAccount() {
+        System.out.println("Full Time Mentor Account is created");
+    }
+}
---
+++ Spring-01-SpringContainer/src/main/java/services/PartTimeMentor.java
@@ -0,0 +1,11 @@
+package services;
+
+import interfaces.Mentor;
+
+public class PartTimeMentor implements Mentor {
+
+
+    public void createAccount() {
+        System.out.println("Part Time Mentor account is created");
+    }
+}
