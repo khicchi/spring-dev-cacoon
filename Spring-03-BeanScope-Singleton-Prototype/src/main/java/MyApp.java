fee85ee0fb4f68d19d1a7ce4d399bd8d6cf38806
ececi <huseyinkiceci@gmail.com>
2021-10-21 15:52:52 +0300

    singleton and prototype scope is checked

--- Spring-03-BeanScope-Singleton-Prototype/src/main/java/MyApp.java
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/MyApp.java
@@ -1,19 +1,35 @@
 import interfaces.Course;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import services.Selenium;
 
 public class MyApp {
+
+    private static ApplicationContext container;
+
     public static void main(String[] args) {
-        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        container = new ClassPathXmlApplicationContext("config.xml");
 
         Course course1 = container.getBean("java",Course.class);
 
         Course course2 = container.getBean("java",Course.class);
-
+        //check java prototype scope
         System.out.println("Pointing to the same object:" + (course1 == course2));
 
-        System.out.println("Memory Location for the course1:" + course1);
+        System.out.println("Memory Location for the course1:" + course1);//Java@3d285d7e
 
-        System.out.println("Memory Location for the course2:" + course2);
+        System.out.println("Memory Location for the course2:" + course2);//Java@40005471
+
+        checkSeleniumSingletonScope();
+    }
+
+    private static void checkSeleniumSingletonScope(){
+        Course selenium1 = container.getBean("selenium", Selenium.class);
+
+        Course selenium2 = container.getBean("selenium",Selenium.class);
+
+        System.out.println("Memory Location for the selenium1:" + selenium1);//Selenium@2cd76f31
+
+        System.out.println("Memory Location for the selenium2:" + selenium2);//Selenium@2cd76f31
     }
 }
