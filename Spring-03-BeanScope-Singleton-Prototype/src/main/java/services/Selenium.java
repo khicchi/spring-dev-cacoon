1d9dd9c3310c4627e0d068b6c33b3de9b7a89e02
ececi <huseyinkiceci@gmail.com>
2021-10-21 15:35:12 +0300

    services finished

---
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/services/Selenium.java
@@ -0,0 +1,10 @@
+package services;
+
+import interfaces.Course;
+
+public class Selenium implements Course {
+    public void getTeachingHours() {
+
+        System.out.println("Weekly Teaching Hours :  15");
+    }
+}
