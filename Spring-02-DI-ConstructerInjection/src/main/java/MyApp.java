3bed2dfdda057b467387b3dc8f678bc02ded2ffa
ececi <huseyinkiceci@gmail.com>
2021-10-21 14:45:14 +0300

    config file added

---
+++ Spring-02-DI-ConstructerInjection/src/main/java/MyApp.java
@@ -0,0 +1,13 @@
+import interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course = container.getBean("java", Course.class);
+
+        course.getTeachingHours();
+    }
+}
