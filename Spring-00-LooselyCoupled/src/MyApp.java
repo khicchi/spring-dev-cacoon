babb758ccd43935dec6f8188006237c88877e662
ececi <huseyinkiceci@gmail.com>
2021-10-21 14:36:24 +0300

    spring container finished

--- Spring-00-LooselyCoupled/src/MyApp.java
+++ Spring-00-LooselyCoupled/src/MyApp.java
@@ -1,12 +1,20 @@
 import service.FullTimeMentor;
+
+import interfaces.Mentor;
 import service.MentorAccount;
+
 
 public class MyApp {
     public static void main(String[] args) {
-        FullTimeMentor fullTimeMentor = new FullTimeMentor();
 
-        MentorAccount mentorAccount = new MentorAccount(fullTimeMentor);
+        FullTimeMentor fullTime = new FullTimeMentor();
+        //PartTimeMentor partTime = new PartTimeMentor();
 
-        mentorAccount.manageAccount();
+        MentorAccount mentor = new MentorAccount(fullTime);
+
+        mentor.manageAccount();
+
     }
+
+
 }
--- Spring-01-SpringContainer/src/main/java/MyApp.java
+++ Spring-01-SpringContainer/src/main/java/MyApp.java
@@ -1,7 +1,26 @@
+import interfaces.Mentor;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MyApp {
 
     public static void main(String[] args) {
 
+        containerFromBeanFactory();
+        containerFromAppContext();
+    }
+
+    static void containerFromBeanFactory(){
+        BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+        Mentor mentor = container.getBean("fullTimeMentor", Mentor.class);
+        mentor.createAccount();
+    }
+
+    static void containerFromAppContext(){
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Mentor mentor = container.getBean("partTimeMentor",Mentor.class);
+        mentor.createAccount();
     }
 
 }
