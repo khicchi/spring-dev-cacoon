6d78854bdc79251466164f416c2811b1528fc603
ececi <huseyinkiceci@gmail.com>
2021-10-21 15:17:19 +0300

    services created

---
+++ Spring-02-DI-SetterInjection/src/main/java/services/OfficeHours.java
@@ -0,0 +1,9 @@
+package services;
+
+import interfaces.ExtraSessions;
+
+public class OfficeHours implements ExtraSessions {
+    public int getHours() {
+       return 5;
+    }
+}
---
+++ Spring-02-DI-SetterInjection/src/main/java/services/Selenium.java
@@ -0,0 +1,9 @@
+package services;
+
+import interfaces.Course;
+
+public class Selenium implements Course {
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours :  15");
+    }
+}
