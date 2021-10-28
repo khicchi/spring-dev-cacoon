ce1045a5ed419518e87f43c132714d68b5dc6c92
ececi <huseyinkiceci@gmail.com>
2021-10-28 16:45:13 +0300

    properties added

--- Spring-07-SpringBoot-Demo/src/main/java/com/lespania/spring07springbootdemo/Spring07SpringBootDemoApplication.java
+++ Spring-07-SpringBoot-Demo/src/main/java/com/lespania/spring07springbootdemo/Spring07SpringBootDemoApplication.java
@@ -1,13 +1,19 @@
 package com.lespania.spring07springbootdemo;
 
+import com.lespania.interfaces.Course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.lespania")
 public class Spring07SpringBootDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring07SpringBootDemoApplication.class, args);
+        ApplicationContext container = SpringApplication.run(Spring07SpringBootDemoApplication.class, args);
+
+        Course course = container.getBean("java", Course.class);
+
+        System.out.println(course.getTeachingHours());
     }
 
 }
