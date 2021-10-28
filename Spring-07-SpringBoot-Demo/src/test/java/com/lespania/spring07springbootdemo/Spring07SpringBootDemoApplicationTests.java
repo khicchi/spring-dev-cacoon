bb83cf762ad132a87607a908b62c45db9df3d100
ececi <huseyinkiceci@gmail.com>
2021-10-28 16:32:52 +0300

    first spring boot app is created

---
+++ Spring-07-SpringBoot-Demo/src/test/java/com/lespania/spring07springbootdemo/Spring07SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lespania.spring07springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
