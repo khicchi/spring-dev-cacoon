73cd8797070a855a33162b175ea7ed760e02a61b
ececi <huseyinkiceci@gmail.com>
2021-10-28 16:33:48 +0300

    interfaces created

---
+++ Spring-07-SpringBoot-Demo/src/main/java/com/lespania/interfaces/Course.java
@@ -0,0 +1,5 @@
+package com.lespania.interfaces;
+
+public interface Course {
+    int getTeachingHours();
+}
---
+++ Spring-07-SpringBoot-Demo/src/main/java/com/lespania/interfaces/ExtraSession.java
@@ -0,0 +1,5 @@
+package com.lespania.interfaces;
+
+public interface ExtraSession {
+    int getHour();
+}
