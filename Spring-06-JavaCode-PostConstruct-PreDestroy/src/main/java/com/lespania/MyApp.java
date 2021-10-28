cb5b456f007644a3cfef697ee00b67ed749b3b9c
ececi <huseyinkiceci@gmail.com>
2021-10-28 13:54:49 +0300

    myapp created

---
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/lespania/MyApp.java
@@ -0,0 +1,21 @@
+package com.lespania;
+
+import com.lespania.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.lespania.config.*;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(MyAppConfig.class);
+
+        Course course = container.getBean("java", Course.class);
+
+        course.getTeachingHours();
+
+        ((AnnotationConfigApplicationContext)container).close();
+
+
+    }
+}
