79c230797c147ed22526d66ff34a3f5339d492e4
ececi <huseyinkiceci@gmail.com>
2021-10-28 13:53:09 +0300

    interfaces and services created

---
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/lespania/services/Selenium.java
@@ -0,0 +1,12 @@
+package com.lespania.services;
+
+import com.lespania.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Selenium implements Course {
+
+    public void getTeachingHours() {
+        System.out.println("Teaching hours : 20");
+    }
+}
