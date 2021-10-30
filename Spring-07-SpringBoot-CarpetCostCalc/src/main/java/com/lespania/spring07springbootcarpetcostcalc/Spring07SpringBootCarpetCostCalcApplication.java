0a3d09466ea85fcb811b104dce7af88d3dfd5288
ececi <huseyinkiceci@gmail.com>
2021-10-30 20:58:26 +0300

    prop added runner applied

--- Spring-07-SpringBoot-CarpetCostCalc/src/main/java/com/lespania/spring07springbootcarpetcostcalc/Spring07SpringBootCarpetCostCalcApplication.java
+++ Spring-07-SpringBoot-CarpetCostCalc/src/main/java/com/lespania/spring07springbootcarpetcostcalc/Spring07SpringBootCarpetCostCalcApplication.java
@@ -1,13 +1,20 @@
 package com.lespania.spring07springbootcarpetcostcalc;
 
+import com.lespania.calculator.Calculator;
+import com.lespania.enums.City;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.lespania")
 public class Spring07SpringBootCarpetCostCalcApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Spring07SpringBootCarpetCostCalcApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        ApplicationContext container = SpringApplication.run(Spring07SpringBootCarpetCostCalcApplication.class, args);
+
+        Calculator calculator = container.getBean("calculator",Calculator.class);
+
+        System.out.println(calculator.getTotalCarpetCost(City.DULLES));
     }
 
 }
