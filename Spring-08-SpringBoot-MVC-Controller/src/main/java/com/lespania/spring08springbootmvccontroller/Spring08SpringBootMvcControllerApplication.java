d19753df0e0391b9410a0cc23fd37ef2c9d70646
ececi <huseyinkiceci@gmail.com>
2021-11-01 16:30:49 +0300

    prop file and runner added

---
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/lespania/spring08springbootmvccontroller/Spring08SpringBootMvcControllerApplication.java
@@ -0,0 +1,13 @@
+package com.lespania.spring08springbootmvccontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08SpringBootMvcControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring08SpringBootMvcControllerApplication.class, args);
+    }
+
+}
