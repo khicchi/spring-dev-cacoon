8ad6c0a9fcd3460251f2c356f06ddf316087fed7
ececi <huseyinkiceci@gmail.com>
2021-11-02 11:44:29 +0300

    thymeleaf started

---
+++ Spring-08-SpringBoot-MVC-Model-View-Thymeleaf/src/main/java/com/lespania/Spring08SpringBootMvcModelViewThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.lespania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08SpringBootMvcModelViewThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring08SpringBootMvcModelViewThymeleafApplication.class, args);
+    }
+
+}
