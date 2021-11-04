a10362ed0be4a2679170da00d56a2be5241edc70
ececi <huseyinkiceci@gmail.com>
2021-11-04 19:22:57 +0300

    emloyee list created

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/lespania/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/lespania/controller/EmployeeController.java
@@ -5,7 +5,12 @@ import com.lespania.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/employee")
@@ -18,6 +23,17 @@ public class EmployeeController {
         model.addAttribute("stateList", DataGenerator.getStateList());
 
         return "employee/employee-create";
+    }
+
+    @PostMapping("/list")
+    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
+
+        model.addAttribute("employeeList", Arrays.asList(employee));
+
+        int birthYear = LocalDate.parse(employee.getBirthday()).getYear();
+        model.addAttribute("age",LocalDate.now().getYear() - birthYear);
+
+        return "employee/employee-list";
     }
 
 }
