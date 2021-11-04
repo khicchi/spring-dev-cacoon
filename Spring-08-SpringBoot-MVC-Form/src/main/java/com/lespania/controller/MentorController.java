f3bdd7095efdb2bb69f1510f88a9e2eeb61c0dbb
ececi <huseyinkiceci@gmail.com>
2021-11-04 12:28:10 +0300

    mentor controller created

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/lespania/controller/MentorController.java
@@ -0,0 +1,28 @@
+package com.lespania.controller;
+
+import com.lespania.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @GetMapping("/register")
+    public String showForm(Model model){
+
+        model.addAttribute("mentor",new Mentor());
+
+        List<String> batchList = Arrays.asList("B7","B8","B9","B10","B11","B12","E1","E2","E3","E4");
+        model.addAttribute("batchList",batchList);
+
+
+        return "mentor/mentor-register";
+    }
+
+}
