fbad67213fee98ceb401054ddec9e0a7af97b551
ececi <huseyinkiceci@gmail.com>
2021-11-04 16:40:11 +0300

    employee controller created

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/lespania/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.lespania.controller;
+
+import com.lespania.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/register")
+    public String employeeCreate(Model model){
+
+        model.addAttribute("employee",new Employee());
+
+
+        return "employee/employee-create";
+    }
+
+}
