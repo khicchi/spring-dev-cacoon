3e9e5af67322d457f6a21cad9b78ac3652664899
ececi <huseyinkiceci@gmail.com>
2021-11-04 16:12:55 +0300

    mentor confirmation applied

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/lespania/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/lespania/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.lespania.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -23,6 +25,15 @@ public class MentorController {
 
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+
+        //System.out.println(mentor.toString());
+
+
+        return "mentor/mentor-confirmation";
     }
 
 }
