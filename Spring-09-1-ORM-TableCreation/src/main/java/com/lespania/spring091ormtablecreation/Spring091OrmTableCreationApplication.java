226c035ab8a329362bd73a8a7c51193cc30099df
ececi <huseyinkiceci@gmail.com>
2021-11-17 23:01:10 +0300

    ORM table mapping started

---
+++ Spring-09-1-ORM-TableCreation/src/main/java/com/lespania/spring091ormtablecreation/Spring091OrmTableCreationApplication.java
@@ -0,0 +1,13 @@
+package com.lespania.spring091ormtablecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring091OrmTableCreationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring091OrmTableCreationApplication.class, args);
+    }
+
+}
