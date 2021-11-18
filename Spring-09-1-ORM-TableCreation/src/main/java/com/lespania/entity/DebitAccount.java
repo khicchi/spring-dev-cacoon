7c23a22617612fac436ae3d51d94565aa4b62833
ececi <huseyinkiceci@gmail.com>
2021-11-18 16:36:11 +0300

    DebitAccount class started

---
+++ Spring-09-1-ORM-TableCreation/src/main/java/com/lespania/entity/DebitAccount.java
@@ -0,0 +1,10 @@
+package com.lespania.entity;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+
+@Entity
+public class DebitAccount extends Account {
+
+    private BigDecimal overDraftFee;
+}
