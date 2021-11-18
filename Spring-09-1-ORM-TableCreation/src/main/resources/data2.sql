e1e2fea3c2b0e45847b8715c1dde8a0c99eb2abd
ececi <huseyinkiceci@gmail.com>
2021-11-18 15:55:25 +0300

    student inserts added

---
+++ Spring-09-1-ORM-TableCreation/src/main/resources/data2.sql
@@ -0,0 +1,4 @@
+INSERT INTO students(first_name,last_name,email) VALUES('Mike','Smith','mike@cybertek.com');
+INSERT INTO students(first_name,last_name,email) VALUES('Adam','Shaun','adam@cybertek.com');
+INSERT INTO students(first_name,last_name,email) VALUES('Tom','Hnaks','tom@cybertek.com');
+INSERT INTO students(first_name,last_name,email) VALUES('Brad','Kelly','brad@cybertek.com');
