a17e8bd5bfeea8330968a2ba8010278fca06601c
ececi <huseyinkiceci@gmail.com>
2021-11-19 20:08:36 +0300

    OneToOneApplication started

---
+++ Spring-09-2-ORM-Mapping-OneToOne/src/main/java/com/lespania/Spring092OrmMappingOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.lespania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring092OrmMappingOneToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring092OrmMappingOneToOneApplication.class, args);
+    }
+
+}
