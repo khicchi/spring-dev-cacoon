3213dc8ce147b376d2bb917b1b05f51466ccb7c0
ececi <huseyinkiceci@gmail.com>
2021-11-20 14:40:43 +0300

    init commit

---
+++ Spring-09--ORM-Mapping-ManyToOne/src/main/java/com/lespania/Spring09OrmMappingManyToOneApplication.java
@@ -0,0 +1,13 @@
+package com.lespania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09OrmMappingManyToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09OrmMappingManyToOneApplication.class, args);
+    }
+
+}
