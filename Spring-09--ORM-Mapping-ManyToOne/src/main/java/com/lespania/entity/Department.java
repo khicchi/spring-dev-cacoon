c99da7e6b9b87736f1a7dd60be63f2e5d6f4d873
ececi <huseyinkiceci@gmail.com>
2021-11-20 14:48:23 +0300

    taken from previous module

---
+++ Spring-09--ORM-Mapping-ManyToOne/src/main/java/com/lespania/entity/Department.java
@@ -0,0 +1,28 @@
+package com.lespania.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Department extends BaseEntity {
+
+    private String department;
+    private String division;
+
+    @OneToOne(mappedBy = "department")
+    private Employee employee;
+
+    public Department(String department, String division) {
+        this.department = department;
+        this.division = division;
+    }
+}
---
+++ Spring-09--ORM-Mapping-ManyToOne/src/main/java/com/lespania/entity/Region.java
@@ -0,0 +1,28 @@
+package com.lespania.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "regions")
+public class Region extends BaseEntity {
+
+    private String region;
+    private String country;
+
+    @OneToOne(mappedBy = "region")
+    private Employee employee;
+
+    public Region(String region, String country) {
+        this.region = region;
+        this.country = country;
+    }
+}
