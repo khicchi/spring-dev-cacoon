75b85c46d76b90838977fa564bceaff3c039e8bf
ececi <huseyinkiceci@gmail.com>
2021-11-20 13:57:52 +0300

    one-to-one mapping and cascading all performed successfully btw emp-dept

---
+++ Spring-09-2-ORM-Mapping-OneToOne/src/main/java/com/lespania/entity/Employee.java
@@ -0,0 +1,47 @@
+package com.lespania.entity;
+
+import com.lespania.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee extends BaseEntity{
+
+    private String firstName, lastName, email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private int salary;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Department department;
+
+    //@OneToOne(cascade = CascadeType.ALL)
+    //@JoinColumn(name = "department_id")
+
+    //
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name="region_id")
+   // private Region region;
+    public Employee(String firstName, String lastName, String email, LocalDate hireDate, Gender gender, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.hireDate = hireDate;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+}
---
+++ Spring-09-2-ORM-Mapping-OneToOne/src/main/java/com/lespania/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.lespania.repository;
+
+import com.lespania.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Integer> {
+}
