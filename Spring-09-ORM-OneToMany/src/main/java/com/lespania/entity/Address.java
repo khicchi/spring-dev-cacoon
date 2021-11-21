2641c9c293b939671a832afd5fbc5962a7db10fd
ececi <huseyinkiceci@gmail.com>
2021-11-21 14:48:36 +0300

    entities created

---
+++ Spring-09-ORM-OneToMany/src/main/java/com/lespania/entity/Address.java
@@ -0,0 +1,30 @@
+package com.lespania.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String zipCode;
+
+    @ManyToOne
+    private Person person;
+
+
+    public Address(String street, String zipCode) {
+        this.street = street;
+        this.zipCode = zipCode;
+    }
+}
---
+++ Spring-09-ORM-OneToMany/src/main/java/com/lespania/entity/Person.java
@@ -0,0 +1,36 @@
+package com.lespania.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(mappedBy = "person")
+    private List<Address> addresses;
+
+
+      //case-1
+//    @OneToMany(cascade = CascadeType.ALL)
+//    @JoinColumn(name="person_id")
+//    private List<Address> addresses;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
