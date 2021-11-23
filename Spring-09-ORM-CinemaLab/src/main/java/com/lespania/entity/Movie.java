c6b8794a8a4dcb8e43cce41d49cbad148722f351
ececi <huseyinkiceci@gmail.com>
2021-11-23 19:52:28 +0300

    movie class almost finished

--- Spring-09-ORM-CinemaLab/src/main/java/com/lespania/entity/Movie.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/lespania/entity/Movie.java
@@ -1,5 +1,45 @@
 package com.lespania.entity;
 
+import com.lespania.enums.MovieState;
+import com.lespania.enums.MovieType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "movie_id")
+    private Long movieId;
+
+    private String name;
+    private BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    private MovieType type;
+    @Enumerated(EnumType.STRING)
+    private MovieState state;
+    @Column(name = "release_date", columnDefinition = "DATE")
+    private LocalDate releaseDate;
+    private Integer duration;
+    @Column(columnDefinition = "text")
+    private String summary;
+
+
+    public Movie(String name, LocalDate releaseDate, Integer duration,MovieType type, MovieState state, BigDecimal price) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+        this.type = type;
+        this.state = state;
+        this.price = price;
+    }
 }
