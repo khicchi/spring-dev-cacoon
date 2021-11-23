52dfd6f59a88707f35b10503c74db342da5b6435
ececi <huseyinkiceci@gmail.com>
2021-11-23 16:28:17 +0300

    cinema lab started

---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/lespania/Spring09OrmCinemaLabApplication.java
@@ -0,0 +1,13 @@
+package com.lespania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09OrmCinemaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09OrmCinemaLabApplication.class, args);
+    }
+
+}
