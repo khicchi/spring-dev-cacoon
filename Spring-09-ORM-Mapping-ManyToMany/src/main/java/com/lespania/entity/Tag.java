9b6b1fac8240543068924ed8fd1f7911bf0a505e
ececi <huseyinkiceci@gmail.com>
2021-11-23 12:44:55 +0300

    tag class cretaed

---
+++ Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/lespania/entity/Tag.java
@@ -0,0 +1,28 @@
+package com.lespania.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+}
