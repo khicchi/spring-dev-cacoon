f225487ad6975cc44a3f7d4f8328bca0a725cb91
ececi <huseyinkiceci@gmail.com>
2021-11-23 19:57:47 +0300

    location class almost finished

---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/lespania/entity/Location.java
@@ -0,0 +1,42 @@
+package com.lespania.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long locationID;
+
+    private String name;
+    private BigDecimal latitude;
+    private BigDecimal langitude;
+    private String postalCode;
+    private String country;
+    private String state;
+    private String city;
+    private String address;
+
+    public Location(String name, BigDecimal latitude, BigDecimal langitude, String postalCode, String country, String state, String city, String address) {
+        this.name = name;
+        this.latitude = latitude;
+        this.langitude = langitude;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.state = state;
+        this.city = city;
+        this.address = address;
+    }
+}
