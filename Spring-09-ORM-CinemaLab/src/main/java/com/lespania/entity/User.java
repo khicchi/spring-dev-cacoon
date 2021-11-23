dc80a863617a90925d8c3a54e67811d73496094a
ececi <huseyinkiceci@gmail.com>
2021-11-23 20:14:54 +0300

    User class almost finished

---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/lespania/entity/User.java
@@ -0,0 +1,30 @@
+package com.lespania.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "user_account")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userAccountId;
+
+    private String email;
+    private String password;
+    private String username;
+
+    public User(String email, String password, String username) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+}
