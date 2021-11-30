480b36951ecd27230350028cb51c20e31bf48710
ececi <huseyinkiceci@gmail.com>
2021-11-30 23:02:19 +0300

    all classes copied from previous project (derived)

---
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/lespania/entity/Employee.java
@@ -0,0 +1,45 @@
+package com.lespania.entity;
+
+import com.lespania.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Employee extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Integer salary;
+
+    @ManyToOne
+    @JoinColumn(name = "department")
+    private Department department;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+}
---
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/lespania/entity/Region.java
@@ -0,0 +1,20 @@
+package com.lespania.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="regions")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Region extends BaseEntity {
+    private String region;
+    private String country;
+}
