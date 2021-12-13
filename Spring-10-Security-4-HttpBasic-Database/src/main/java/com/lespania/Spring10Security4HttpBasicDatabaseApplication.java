8b9e1e48de63a6cce33530750ff549ab458cb969
ececi <huseyinkiceci@gmail.com>
2021-12-13 20:24:59 +0300

    http security with db conn just started

---
+++ Spring-10-Security-4-HttpBasic-Database/src/main/java/com/lespania/Spring10Security4HttpBasicDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.lespania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10Security4HttpBasicDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring10Security4HttpBasicDatabaseApplication.class, args);
+    }
+
+}
