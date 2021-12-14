85da57efbe7473f2c2d891f427a1e3f0254bf99e
ececi <huseyinkiceci@gmail.com>
2021-12-14 12:38:40 +0300

    controllers created

---
+++ Spring-10-Security-5-FormAuth/src/main/java/com/lespania/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.lespania.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/index")
+    public String index(){
+
+        return "/index";
+    }
+
+    @GetMapping(value = {"/login","/"})
+    public String login(){
+        return "login";
+    }
+}
