1ccf281f86ab6eb2e90a7a35dad2bfbded547e00
ececi <huseyinkiceci@gmail.com>
2021-12-21 23:45:24 +0300

    getForObject is finished

--- Spring-11-Rest-5-RestTemplate/src/main/java/com/lespania/controller/HomeController.java
+++ Spring-11-Rest-5-RestTemplate/src/main/java/com/lespania/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.lespania.controller;
 import com.lespania.entity.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +22,12 @@ public class HomeController {
     public User[] readAllUsers(){
         ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(URI, User[].class);
         return responseEntity.getBody();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Object readUser(@PathVariable("id") Integer id){
+        String URL = URI + "/{id}";
+        return restTemplate.getForObject(URL,Object.class,id);
     }
 
 }
