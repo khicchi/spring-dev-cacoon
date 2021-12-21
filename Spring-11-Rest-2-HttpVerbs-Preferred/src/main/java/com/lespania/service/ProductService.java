ecd3eeae74cf0feaf5fa2bcf8c85f069b37680f5
ececi <huseyinkiceci@gmail.com>
2021-12-21 12:52:02 +0300

    just copied from legacy

---
+++ Spring-11-Rest-2-HttpVerbs-Preferred/src/main/java/com/lespania/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.lespania.service;
+
+import com.lespania.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getProducts();
+    List<Product> delete(long id);
+    List<Product> updateProduct(long id,Product product);
+    List<Product> createProduct(Product product);
+    Product getProduct(long id);
+
+}
