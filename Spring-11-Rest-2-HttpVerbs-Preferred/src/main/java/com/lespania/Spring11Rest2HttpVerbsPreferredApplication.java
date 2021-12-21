74ba889b041805a68cbe25f3f17502b1bfa5cf8e
ececi <huseyinkiceci@gmail.com>
2021-12-21 12:50:36 +0300

    preferred just started

---
+++ Spring-11-Rest-2-HttpVerbs-Preferred/src/main/java/com/lespania/Spring11Rest2HttpVerbsPreferredApplication.java
@@ -0,0 +1,13 @@
+package com.lespania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11Rest2HttpVerbsPreferredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring11Rest2HttpVerbsPreferredApplication.class, args);
+    }
+
+}
