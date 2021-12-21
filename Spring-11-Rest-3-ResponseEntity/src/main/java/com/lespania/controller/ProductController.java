7e119261fda5653faed85b8bf7d37357089ca6e3
ececi <huseyinkiceci@gmail.com>
2021-12-21 16:02:17 +0300

    performed different header creations

--- Spring-11-Rest-3-ResponseEntity/src/main/java/com/lespania/controller/ProductController.java
+++ Spring-11-Rest-3-ResponseEntity/src/main/java/com/lespania/controller/ProductController.java
@@ -3,41 +3,82 @@ package com.lespania.controller;
 
 import com.lespania.entity.Product;
 import com.lespania.service.ProductService;
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
     private ProductService productService;
+
     public ProductController(ProductService productService) {
-        this.productService = productService;}
-
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.GET)
-    public @ResponseBody Product getProduct(@PathVariable("id") long id){
-        return productService.getProduct(id);
+        this.productService = productService;
     }
 
-    @RequestMapping(value = "/products")
-    public @ResponseBody List<Product> getProducts(){
-        return productService.getProducts();
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public ResponseEntity<Product> getProduct(@PathVariable("id") long id){
+        return ResponseEntity.ok(productService.getProduct(id));
     }
 
-    @RequestMapping(value = "/products",method = RequestMethod.POST)
-    public @ResponseBody List<Product> createProduct(@RequestBody Product product){
-        return productService.createProduct(product);
+    @GetMapping
+    public  ResponseEntity<List<Product>> getProducts(){
+        HttpHeaders responseHttpHeaders = new HttpHeaders();
+        responseHttpHeaders.set("Version","Cybertek.v1");
+        responseHttpHeaders.set("Operation","Get List");
+
+        return ResponseEntity
+                .ok()
+                .headers(responseHttpHeaders)
+                .body(productService.getProducts());
     }
 
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.DELETE)
-    public @ResponseBody List<Product> deleteProduct(@PathVariable("id") long id){
-        return productService.delete(id);
+    @PostMapping
+    public  ResponseEntity<List<Product>> createProduct(@RequestBody Product product){
+
+        List<Product> set = productService.createProduct(product);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version","Cybertek.V1")
+                .header("Operation","Create")
+                .body(set);
     }
 
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.PUT)
-    public @ResponseBody List<Product> updateProduct(@PathVariable("id") long id,@RequestBody Product product){
-        return productService.updateProduct(id, product);
+    @DeleteMapping(value = "/{id}")
+    public  ResponseEntity<List<Product>> deleteProduct(@PathVariable("id") long id){
+
+        HttpHeaders responseHttpHeaders = new HttpHeaders();
+        responseHttpHeaders.set("Version","Cybertek.v1");
+        responseHttpHeaders.set("Operation","Delete");
+
+        List<Product> list = productService.delete(id);
+
+        return new ResponseEntity<>(list,responseHttpHeaders, HttpStatus.OK);
     }
+
+    @PutMapping(value = "/{id}")
+    public  ResponseEntity<List<Product>> updateProduct(@PathVariable("id") long id,@RequestBody Product product){
+
+        MultiValueMap<String,String> map = new LinkedMultiValueMap<>();
+        map.add("Version","Cybertek.V1");
+        map.add("Operation","Update");
+
+        List<Product> list = productService.updateProduct(id,product);
+
+        return new ResponseEntity<>(list,map,HttpStatus.OK);
+
+    }
+
+
 
 }
 
