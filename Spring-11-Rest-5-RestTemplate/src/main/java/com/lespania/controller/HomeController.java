b7221b7a0e3baff8fc892134dade3e7ee700a8b6
ececi <huseyinkiceci@gmail.com>
2021-12-21 23:51:03 +0300

    exchange is finished

--- Spring-11-Rest-5-RestTemplate/src/main/java/com/lespania/controller/HomeController.java
+++ Spring-11-Rest-5-RestTemplate/src/main/java/com/lespania/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.lespania.controller;
 
 import com.lespania.entity.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @RestController
 public class HomeController {
@@ -28,6 +30,22 @@ public class HomeController {
     public Object readUser(@PathVariable("id") Integer id){
         String URL = URI + "/{id}";
         return restTemplate.getForObject(URL,Object.class,id);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumePostsFromDummyApi(){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","lTE5abbDxdjGplutvTuc");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/api/user?limit=10",
+                HttpMethod.GET,entity,Object.class);
+
+        return response;
+
     }
 
 }
