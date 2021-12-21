2f2d66dc86c30168d8f456b09b40fbcbafc6a034
ececi <huseyinkiceci@gmail.com>
2021-12-21 15:25:50 +0300

    base structure copied from legacy module

---
+++ Spring-11-Rest-3-ResponseEntity/src/main/java/com/lespania/controller/ProductController.java
@@ -0,0 +1,56 @@
+package com.lespania.controller;
+
+
+import com.lespania.entity.Product;
+import com.lespania.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+    private ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;}
+
+    @RequestMapping(value = "/products/{id}",method = RequestMethod.GET)
+    public @ResponseBody Product getProduct(@PathVariable("id") long id){
+        return productService.getProduct(id);
+    }
+
+    @RequestMapping(value = "/products")
+    public @ResponseBody List<Product> getProducts(){
+        return productService.getProducts();
+    }
+
+    @RequestMapping(value = "/products",method = RequestMethod.POST)
+    public @ResponseBody List<Product> createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+
+    @RequestMapping(value = "/products/{id}",method = RequestMethod.DELETE)
+    public @ResponseBody List<Product> deleteProduct(@PathVariable("id") long id){
+        return productService.delete(id);
+    }
+
+    @RequestMapping(value = "/products/{id}",method = RequestMethod.PUT)
+    public @ResponseBody List<Product> updateProduct(@PathVariable("id") long id,@RequestBody Product product){
+        return productService.updateProduct(id, product);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
