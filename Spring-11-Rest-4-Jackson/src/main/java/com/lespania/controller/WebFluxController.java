8d06e492a333d20d008a915338604c89aa0b37c1
ececi <huseyinkiceci@gmail.com>
2021-12-22 22:31:09 +0300

    first flux endpoint created

--- Spring-11-Rest-4-Jackson/src/main/java/com/lespania/controller/WebFluxController.java
+++ Spring-11-Rest-4-Jackson/src/main/java/com/lespania/controller/WebFluxController.java
@@ -1,7 +1,27 @@
 package com.lespania.controller;
 
+import com.lespania.entity.MovieCinema;
+import com.lespania.repository.GenreRepository;
+import com.lespania.repository.MovieCinemaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class WebFluxController {
+    private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080").build();
+
+    private MovieCinemaRepository movieCinemaRepository;
+    private GenreRepository genreRepository;
+
+    public WebFluxController(MovieCinemaRepository movieCinemaRepository, GenreRepository genreRepository) {
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.genreRepository = genreRepository;
+    }
+
+    @GetMapping("/flux-movie-cinemas")
+    public Flux<MovieCinema> readAllCinemaFlux(){
+        return Flux.fromIterable(movieCinemaRepository.findAll());
+    }
 }
