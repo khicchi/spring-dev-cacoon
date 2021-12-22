2e8eae8837f79583fc13a637edef6897ce3804bc
ececi <huseyinkiceci@gmail.com>
2021-12-22 22:31:53 +0300

    first mono endpoint created

--- Spring-11-Rest-4-Jackson/src/main/java/com/lespania/controller/WebFluxController.java
+++ Spring-11-Rest-4-Jackson/src/main/java/com/lespania/controller/WebFluxController.java
@@ -4,9 +4,11 @@ import com.lespania.entity.MovieCinema;
 import com.lespania.repository.GenreRepository;
 import com.lespania.repository.MovieCinemaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class WebFluxController {
@@ -24,4 +26,10 @@ public class WebFluxController {
     public Flux<MovieCinema> readAllCinemaFlux(){
         return Flux.fromIterable(movieCinemaRepository.findAll());
     }
+
+    @GetMapping("/mono-movie-cinema/{id}")
+    public Mono<MovieCinema> readyById(@PathVariable("id") Long id){
+        return Mono.just(movieCinemaRepository.findById(id).get());
+    }
+
 }
