35664abbad4b763aa3809bf47ac57a756013f209
ececi <huseyinkiceci@gmail.com>
2021-12-22 20:45:14 +0300

    redAllTeachers finished

--- Spring-11-Rest-6-SchoolLab/src/main/java/com/lespania/controller/ApiController.java
+++ Spring-11-Rest-6-SchoolLab/src/main/java/com/lespania/controller/ApiController.java
@@ -1,10 +1,14 @@
 package com.lespania.controller;
 
+import com.lespania.model.Teacher;
 import com.lespania.repository.AddressRepository;
 import com.lespania.repository.ParentRepository;
 import com.lespania.repository.StudentRepository;
 import com.lespania.repository.TeacherRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ApiController {
@@ -19,6 +23,11 @@ public class ApiController {
         this.studentRepository = studentRepository;
         this.parentRepository = parentRepository;
         this.addressRepository = addressRepository;
+    }
+
+    @GetMapping("/teachers")
+    public List<Teacher> redAllTeachers(){
+        return teacherRepository.findAll();
     }
 
 }
