bca5d2d2b0cf3685dc329ebdb054196af231e850
ececi <huseyinkiceci@gmail.com>
2021-12-25 15:59:52 +0300

    basic files copied from cinema lab module

--- Spring-11-Rest-70-OpenApi3/src/main/java/com/lespania/Spring11Rest70OpenApi3Application.java
+++ Spring-11-Rest-70-OpenApi3/src/main/java/com/lespania/Spring11Rest70OpenApi3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring117RestOpenApi3Application {
+public class Spring11Rest70OpenApi3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring117RestOpenApi3Application.class, args);
+        SpringApplication.run(Spring11Rest70OpenApi3Application.class, args);
     }
 
 }
