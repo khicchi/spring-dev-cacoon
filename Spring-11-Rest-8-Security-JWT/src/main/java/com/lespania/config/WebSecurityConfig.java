27cb2d20d2875e1f4efbab4b52f369257b542a44
ececi <huseyinkiceci@gmail.com>
2021-12-27 16:34:03 +0300

    WebSecurityConfig created

---
+++ Spring-11-Rest-8-Security-JWT/src/main/java/com/lespania/config/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.lespania.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    private static final String[] permittedUrls ={
+            "/authenticate",
+            "/create-user",
+            "/api/p1/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/webjars/**",
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+        http
+                .csrf()             //cross site request forgery
+                .disable()          //disables csrf
+                .authorizeRequests()
+                .antMatchers(permittedUrls)
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
