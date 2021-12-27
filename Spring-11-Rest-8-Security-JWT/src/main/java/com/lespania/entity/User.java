956f190ce786ff511e1decb9fbdc10b6dbce5dc3
ececi <huseyinkiceci@gmail.com>
2021-12-27 16:13:10 +0300

    User class created

---
+++ Spring-11-Rest-8-Security-JWT/src/main/java/com/lespania/entity/User.java
@@ -0,0 +1,38 @@
+package com.lespania.entity;
+
+import com.lespania.enums.UserRole;
+import com.lespania.enums.UserState;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@Where(clause = "is_deleted=false")
+public class User extends BaseEntity {
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private Boolean isVerified;
+
+    @Enumerated(EnumType.STRING)
+    private UserState state;
+}
