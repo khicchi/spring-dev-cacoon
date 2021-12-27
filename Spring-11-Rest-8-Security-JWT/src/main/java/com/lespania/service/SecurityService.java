63e2a2277da7abc7ac25373669470d35c50bcf7d
ececi <huseyinkiceci@gmail.com>
2021-12-27 16:42:36 +0300

    SecurityService created

---
+++ Spring-11-Rest-8-Security-JWT/src/main/java/com/lespania/service/SecurityService.java
@@ -0,0 +1,47 @@
+package com.lespania.service;
+
+import com.lespania.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SecurityService implements UserDetailsService {
+
+    private UserService userService;
+
+    public SecurityService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        User foundUser = loadUser(s);
+        if(foundUser == null){
+            throw new UsernameNotFoundException("user not found! " + s);
+        }
+
+        return new org.springframework.security.core.userdetails.User(foundUser.getUsername(), foundUser.getPassword(), listAuthorities(foundUser));
+    }
+
+    public User loadUser(String value){
+        boolean isEmail = value.contains("@");
+        return isEmail ? userService.readByEmail(value) : userService.readByUsername(value);
+    }
+
+    private List<GrantedAuthority> listAuthorities(User user){
+        List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
+        grantedAuthorityList.add(new SimpleGrantedAuthority(user.getRole().name()));
+        return grantedAuthorityList;
+    }
+
+
+}
