016f37bbfab01a362941cde58b50499986214800
ececi <huseyinkiceci@gmail.com>
2021-12-28 12:35:03 +0300

    UserController created yml file updated

---
+++ Spring-11-Rest-8-Security-JWT/src/main/java/com/lespania/controller/AuthenticationController.java
@@ -0,0 +1,54 @@
+package com.lespania.controller;
+
+import com.lespania.entity.AuthenticationRequest;
+import com.lespania.entity.ResponseWrapper;
+import com.lespania.entity.User;
+import com.lespania.exception.ServiceException;
+import com.lespania.service.UserService;
+import com.lespania.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JWTUtil jwtUtil;
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest){
+
+        String password = authenticationRequest.getPassword();
+        String username = authenticationRequest.getUsername();
+
+        User foundUser = userService.readByUsername(username);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,password);
+        authenticationManager.authenticate(authenticationToken);
+
+        String jwtToken = jwtUtil.generateToken(foundUser);
+
+        return ResponseEntity.ok(new ResponseWrapper("Login Succesfull!",jwtToken));
+
+    }
+
+    @PostMapping("/create-user")
+    public ResponseEntity<ResponseWrapper> createAccount(@RequestBody User user) throws ServiceException {
+
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.ok(new ResponseWrapper("User has been created successfully",createdUser));
+
+    }
+
+
+}
---
+++ Spring-11-Rest-8-Security-JWT/src/main/java/com/lespania/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.lespania.controller;
+
+import com.lespania.entity.ResponseWrapper;
+import com.lespania.entity.User;
+import com.lespania.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/read")
+    @PreAuthorize("hasAuthority('USER')")
+//    @PreAuthorize("hasAnyAuthority('USER','ADMIN')")
+    public ResponseEntity<ResponseWrapper> readAll(){
+
+        List<User> users = userService.getAll();
+
+        return ResponseEntity.ok(new ResponseWrapper("Done",users));
+
+    }
+}
