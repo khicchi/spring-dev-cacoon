3dcc74e4c622436a7dbc5c5db37ceed6243b9044
ececi <huseyinkiceci@gmail.com>
2021-12-30 10:06:05 +0300

    some openapi stuff added

--- Spring-11-Rest-8-Security-JWT/src/main/java/com/lespania/controller/AuthenticationController.java
+++ Spring-11-Rest-8-Security-JWT/src/main/java/com/lespania/controller/AuthenticationController.java
@@ -7,6 +7,8 @@ import com.lespania.entity.User;
 import com.lespania.exception.ServiceException;
 import com.lespania.service.UserService;
 import com.lespania.util.JWTUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authenticate controller",description = "Authenticate API")
 @RestController
 public class AuthenticationController {
 
@@ -27,6 +30,7 @@ public class AuthenticationController {
 
     @DefaultExceptionMessage(defaultMessage = "Hey, bad credentials :(")
     @PostMapping("/authenticate")
+    @Operation(summary = "Login to application")
     public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest){
 
         String password = authenticationRequest.getPassword();
@@ -43,6 +47,8 @@ public class AuthenticationController {
 
     }
 
+    @DefaultExceptionMessage(defaultMessage = "Failed to crate user, please try again")
+    @Operation(summary = "Create a new user")
     @PostMapping("/create-user")
     public ResponseEntity<ResponseWrapper> createAccount(@RequestBody User user) throws ServiceException {
 
