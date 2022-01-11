fa12d05ad31cfd2ed5477f2cbca7f01b8211ce18
ececi <huseyinkiceci@gmail.com>
2022-01-11 22:19:11 +0300

    Spring12AopApplication just started

---
+++ Spring-12-AOP/src/main/java/com/lespania/Spring12AopApplication.java
@@ -0,0 +1,13 @@
+package com.lespania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring12AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring12AopApplication.class, args);
+    }
+
+}
