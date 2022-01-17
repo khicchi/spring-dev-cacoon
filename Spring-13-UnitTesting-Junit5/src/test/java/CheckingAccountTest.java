526965c4a1aff3fbe1530023de5c27c058bbf2f5
ececi <huseyinkiceci@gmail.com>
2022-01-17 21:50:02 +0300

    CheckingAccountTest test class created

---
+++ Spring-13-UnitTesting-Junit5/src/test/java/CheckingAccountTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckingAccountTest {
+
+    CheckingAccount checkingAccount;
+
+    @BeforeEach
+    void setUp() {
+        checkingAccount=new CheckingAccount();
+        checkingAccount.setInfo(100,12345476L,"Ozzy");
+    }
+
+    @Test
+    void deposit() {
+        assertEquals(200,checkingAccount.deposit(100));
+    }
+
+    @Test
+    void withdraw() {
+        assertEquals(80,checkingAccount.withdraw(20));
+    }
+
+    @Test
+    void purchase() {
+        assertEquals(-65,checkingAccount.purchase("Shoes",130));
+    }
+
+    @Test
+    void withdraw_branch(){
+        assertThrows(IllegalArgumentException.class,() ->{
+            checkingAccount.withdraw_branch(600,false);
+        });
+    }
+}
