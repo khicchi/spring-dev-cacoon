b44e64e77dc094274d125320d67467b9891c32fa
ececi <huseyinkiceci@gmail.com>
2022-01-18 15:24:41 +0300

    runners added

---
+++ Spring-13-UnitTesting-MockMvc/src/main/java/com/lespania/Spring13UnitTestingMockMvcApplication.java
@@ -0,0 +1,13 @@
+package com.lespania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13UnitTestingMockMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring13UnitTestingMockMvcApplication.class, args);
+    }
+
+}
---
+++ Spring-13-UnitTesting-MockMvc/src/test/java/com/lespania/Spring13UnitTestingMockMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lespania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring13UnitTestingMockMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
