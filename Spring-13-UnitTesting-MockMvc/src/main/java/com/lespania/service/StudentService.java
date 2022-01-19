c59695a86f7937733e4aae11e89adf338b3ac615
ececi <huseyinkiceci@gmail.com>
2022-01-19 12:03:44 +0300

    Student repo and service created

---
+++ Spring-13-UnitTesting-MockMvc/src/main/java/com/lespania/service/StudentService.java
@@ -0,0 +1,24 @@
+package com.lespania.service;
+
+import com.lespania.entity.Student;
+import com.lespania.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudent_data(){
+        return studentRepository.findAll();
+    }
+
+
+}
