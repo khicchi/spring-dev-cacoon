a7d5b0711ef10307010146d2aa5969c9f681fd77
ececi <huseyinkiceci@gmail.com>
2022-01-19 12:17:26 +0300

    StudentControllerTest created

---
+++ Spring-13-UnitTesting-MockMvc/src/test/java/com/lespania/controller/StudentControllerTest.java
@@ -0,0 +1,93 @@
+package com.lespania.controller;
+
+import com.lespania.entity.Student;
+import com.lespania.service.StudentService;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(StudentController.class)
+class StudentControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    StudentService studentService;
+
+    @Test
+    void getStudent_service() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/student").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\": 0,\"firstName\": \"Mike\",\"lastName\": \"Smith\",\"age\": 20}"))
+                .andReturn();
+    }
+
+    @Test
+    void jsonAssert() throws JSONException {
+
+        String actual = "{\"id\": 0,\"firstName\": \"Mike\",\"lastName\": \"Smith\",\"age\": 20}";
+        String expected = "{\"id\": 0,\"firstName\": \"Mike\",\"lastName\": \"Smith\"}";
+
+        JSONAssert.assertEquals(expected,actual,false);
+    }
+
+    @Test
+    void jsonAssert_withoutEscapeCharacters() throws JSONException {
+
+        String actual = "{id: 0,firstName:Mike,lastName:Smith,age:20}";
+        String expected = "{id:0,firstName:Mike,lastName:Smith}";
+
+        JSONAssert.assertEquals(expected,actual,false);
+    }
+
+    @Test
+    void getStudent_data() throws Exception {
+
+        when(studentService.getStudent_data()).thenReturn(Arrays.asList(
+                new Student("ozzy","can",20),
+                new Student("tom","hanks",50)
+        ));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/data").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\":0,\"firstName\":\"ozzy\",\"lastName\":\"can\",\"age\":20},{\"id\":0,\"firstName\":\"tom\",\"lastName\":\"hanks\",\"age\":50}]"))
+                .andReturn();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
