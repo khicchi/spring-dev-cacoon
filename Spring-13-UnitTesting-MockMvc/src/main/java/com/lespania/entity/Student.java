205d572af1ca0a5c4d6ede47c1bee071bf6a1dc4
ececi <huseyinkiceci@gmail.com>
2022-01-19 12:02:19 +0300

    Student class created

---
+++ Spring-13-UnitTesting-MockMvc/src/main/java/com/lespania/entity/Student.java
@@ -0,0 +1,32 @@
+package com.lespania.entity;
+
+import com.sun.xml.bind.v2.model.core.ID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
